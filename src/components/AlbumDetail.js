c7d38850f42204d31bb570c32c885da784818154
Denis Molloy <denis.molloy@gmail.com>
2019-04-28 13:08:58 -0700

    finished albums app

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, Linking } from "react-native";
 import Card from "./Card";
 import CardSection from "./CardSection";
+import Button from "./Button";
 export default function AlbumDetail({ album }) {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const {
     thumbnailStyle,
     headerContentStyle,
@@ -23,8 +24,13 @@ export default function AlbumDetail({ album }) {
           <Text>{artist}</Text>
         </View>
       </CardSection>
+
       <CardSection>
         <Image source={{ uri: image }} style={mainImageStyle} />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>Purchase Album</Button>
       </CardSection>
     </Card>
   );
